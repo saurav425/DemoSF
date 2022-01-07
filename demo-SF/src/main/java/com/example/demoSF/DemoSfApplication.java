57bc9258b0826b471262f082eca95c50ac25c2e8
Sushant Saurav <sushant.saurav@wissen.com>
2022-01-07 11:55:01 +0530

    initial commit

---
+++ demo-SF/src/main/java/com/example/demoSF/DemoSfApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoSF;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSfApplication.class, args);
+	}
+
+}
---
+++ demo-SF/src/test/java/com/example/demoSF/DemoSfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoSF;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
